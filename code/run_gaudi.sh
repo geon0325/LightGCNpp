6a030e2f4214d35c5e7b40bec3670edcdbe672a9
bokveizen <boqvezen97@kaist.ac.kr>
2024-11-04 15:47:02 +0900

    Create run_gaudi.sh

---
+++ code/run_gaudi.sh
@@ -0,0 +1,5 @@
+PT_HPU_LAZY_MODE=0 python main_gaudi.py --dataset="lastfm" --alpha 0.6 --beta -0.1 --gamma 0.2 
+PT_HPU_LAZY_MODE=0 python main_gaudi.py --dataset="ml-1m" --alpha 0.5 --beta 0.0 --gamma 0.0 
+PT_HPU_LAZY_MODE=0 python main_gaudi.py --dataset="gowalla" --alpha 0.6 --beta -0.1 --gamma 0.2 
+PT_HPU_LAZY_MODE=0 python main_gaudi.py --dataset="yelp2018" --alpha 0.6 --beta -0.1 --gamma 0.1
+PT_HPU_LAZY_MODE=0 python main_gaudi.py --dataset="amazon-book" --alpha 0.6 --beta -0.1 --gamma 0.2 
