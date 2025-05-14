4de200e84c424eecc585540f3ac6800d71456891
Geon Lee <geonlee0325@kaist.ac.kr>
2025-05-13 17:16:01 -0700

    Update run.sh

--- SELFRec/run.sh
+++ SELFRec/run.sh
@@ -1,5 +1,6 @@
-gpu=0
+#!/bin/bash
 
+gpu=0
 model_name=LightGCNpp
 model_type=graph
 
@@ -9,14 +10,38 @@ epoch=200
 batch_size=2048
 learning_rate=0.001
 reg_lambda=0.0001
-
 n_layer=2
 
-dataset=lastfm
-alpha=0.6
-beta=-0.1
-gamma=0.0
+# Take dataset as argument
+dataset=$1
 
+# Set alpha, beta, gamma based on dataset
+case $dataset in
+    lastfm)
+        alpha=0.6; beta=-0.1; gamma=0.0;;
+    citeulike)
+        alpha=0.5; beta=-0.1; gamma=0.4;;
+    movielens-1m)
+        alpha=0.4; beta=0.1; gamma=0.0;;
+    gowalla)
+        alpha=0.6; beta=-0.1; gamma=0.2;;
+    yelp)
+        alpha=0.6; beta=-0.1; gamma=0.0;;
+    amazon-sports)
+        alpha=0.6; beta=-0.1; gamma=0.0;;
+    amazon-beauty)
+        alpha=0.5; beta=0.0; gamma=0.2;;
+    amazon-book)
+        alpha=0.6; beta=-0.1; gamma=0.2;;
+    movielens-10m)
+        alpha=0.6; beta=-0.1; gamma=0.0;;
+    alibaba)
+        alpha=0.6; beta=0.0; gamma=0.1;;
+    *)
+        echo "Unknown dataset: $dataset"; exit 1;;
+esac
+
+# Run
 python main.py \
     --gpu $gpu \
     --dataset $dataset \
@@ -31,4 +56,4 @@ python main.py \
     --n_layer $n_layer \
     --alpha $alpha \
     --beta $beta \
-    --gamma $gamma 
+    --gamma $gamma
